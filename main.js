fb80a549390ccdc28a04ea202be9592112af8817
willy0483 <willyjensen251@gmail.com>
2024-08-05 17:20:05 +0200

    .

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 document.addEventListener('scroll', () => {
-    requestAnimationFrame(updateProgressBar);
+    requestAnimationFrame(() => {
+        const scrollTop = window.scrollY;
+        const windowHeight = document.documentElement.scrollHeight - window.innerHeight;
+        const scrolled = (scrollTop / windowHeight) * 100;
+        document.getElementById('progress-bar').style.width = scrolled;
+    });
 });
-
-function updateProgressBar() {
-    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
-    const clientHeight = document.documentElement.clientHeight;
-    const windowHeight = scrollHeight - clientHeight;
-    const scrolled = (scrollTop / windowHeight) * 100;
-
-    document.getElementById('progress-bar').style.width = scrolled + '%';
-}
